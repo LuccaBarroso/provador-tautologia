652ffbd4775c2fd595e9aa457162ca331c30106f
Lucca Barroso <barrosolucca@gmail.com>
2022-05-20 12:07:09 -0300

    style: background
    displayed the background img and also the front card

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="contentArea">
+      <input-form />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputForm from "./components/InputForm.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { InputForm },
+  name: "App",
+};
 </script>
 
-<style>
+<style lang="scss">
+* {
+  margin: 0;
+  overflow-x: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: url(assets/background.svg) repeat left top;
+  background-size: 250px;
+  width: 100vw;
+  min-height: 106vh;
+  overflow-x: hidden;
+  background-color: #0e4a57;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  #contentArea {
+    overflow: hidden;
+    width: 80vw;
+    background-color: white;
+    border-radius: 5px;
+    margin-top: 3vh;
+    margin-bottom: 3vh;
+    max-width: 1800px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+      rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+  }
+}
+@media (max-width: 800px) {
+  #contentArea {
+    width: 95vw !important;
+  }
 }
 </style>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="inputTautologia">
+    <input type="text" id="fname" name="fname" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
